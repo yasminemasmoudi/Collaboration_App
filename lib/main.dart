2060c882274a7fbc3b7d3222bfa55d7014bdce96
sarra zaghbib <sarrazaghbib20@gmail.com>
2022-11-23 17:19:04 +0100

    ss

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: reminders(),
+      home: projectHome(),
     );
   }
 }
