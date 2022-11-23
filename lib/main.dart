4994efa871a6faf6423ba33f5254b6120582eeb6
Yasmine Masmoudi <87315006+yasminemasmoudi@users.noreply.github.com>
2022-11-23 01:40:59 +0100

    yasmine drawboard

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:collabapp/screens/OTP/Phone.dart';
 import 'package:collabapp/screens/OTP/Verif.dart';
 import 'package:collabapp/screens/Onboarding/onboarding.dart';
 import 'package:collabapp/screens/Splash/splashscreen.dart';
+import 'package:collabapp/screens/draw.dart';
 import 'package:collabapp/screens/wrapper.dart';
 import 'package:collabapp/screens/Events/event.dart';
 import 'package:collabapp/screens/Calender/calender_view.dart';
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: DrawingBoard(),
     );
   }
 }
