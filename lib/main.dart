6fde16aff1d360cd815249182b117e433c4e906b
Yasmine Masmoudi <87315006+yasminemasmoudi@users.noreply.github.com>
2022-12-20 11:48:42 +0100

    last

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:collabapp/screens/Calender/calender_view.dart';
 import 'package:collabapp/screens/Home/home.dart';
 import 'package:collabapp/screens/Login/login.dart';
@@ -10,10 +12,21 @@ import 'package:collabapp/screens/projectsView.dart';
 import 'package:collabapp/screens/reminders.dart';
 import 'package:collabapp/screens/wrapper.dart';
 import 'package:collabapp/screens/Events/event_list.dart';
-import 'package:collabapp/screens/Calender/calender_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "",
+    appId: "1:826791224015:android:980ef2006c91e7c19c4dba",
+    messagingSenderId: "",
+    projectId: "collabapp-1567f",
+  ));
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
