6441bca570664a38ec17b73afec67f89b2f60123
Yasmine Masmoudi <87315006+yasminemasmoudi@users.noreply.github.com>
2022-12-26 11:09:32 +0100

    phone

--- lib/screens/OTP/Phone.dart
+++ lib/screens/OTP/Phone.dart
@@ -1,8 +1,11 @@
 import 'package:collabapp/resources/color_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MyPhone extends StatefulWidget {
   const MyPhone({Key? key}) : super(key: key);
+
+  static String verify = "";
 
   @override
   State<MyPhone> createState() => _MyPhoneState();
@@ -10,6 +13,8 @@ class MyPhone extends StatefulWidget {
 
 class _MyPhoneState extends State<MyPhone> {
   TextEditingController countryController = TextEditingController();
+
+  var phone = "";
 
   @override
   void initState() {
@@ -80,8 +85,9 @@ class _MyPhoneState extends State<MyPhone> {
                     const SizedBox(
                       width: 10,
                     ),
-                    const Expanded(
+                    Expanded(
                         child: TextField(
+                      onChanged: (value) => phone = value,
                       keyboardType: TextInputType.phone,
                       decoration: InputDecoration(
                         border: InputBorder.none,
@@ -102,8 +108,18 @@ class _MyPhoneState extends State<MyPhone> {
                         backgroundColor: ColorManager.primary,
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(10))),
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'verify');
+                    onPressed: () async {
+                      await FirebaseAuth.instance.verifyPhoneNumber(
+                        phoneNumber: countryController.text + phone,
+                        verificationCompleted:
+                            (PhoneAuthCredential credential) {},
+                        verificationFailed: (FirebaseAuthException e) {},
+                        codeSent: (String verificationId, int? resendToken) {
+                          MyPhone.verify = verificationId;
+                          Navigator.pushNamed(context, "/verify");
+                        },
+                        codeAutoRetrievalTimeout: (String verificationId) {},
+                      );
                     },
                     child: const Text("Send the code")),
               )
